d10d0e7468e5e24ca12b15f87d6af087dbfef600
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2024-10-22 06:35:33 +0700

    add fillable content

--- app/Models/OfficeSpace.php
+++ app/Models/OfficeSpace.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OfficeSpace extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'thumbnail',
+        'is_open',
+        'is_full_booked',
+        'price',
+        'duration',
+        'about',
+        'slug',
+        'city_id',
+    ];
 }
