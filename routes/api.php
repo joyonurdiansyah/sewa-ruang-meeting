705e325459a926480afd504225a337c4613146e3
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2024-10-23 05:27:53 +0700

    feature add middleware api

--- routes/api.php
+++ routes/api.php
@@ -21,11 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/city/{city:slug}', [CityController::class, 'show']);
-Route::apiResource('/cities', CityController::class);
+Route::middleware('api_key')->group(function () {
+    Route::get('/city/{city:slug}', [CityController::class, 'show']);
+    Route::apiResource('/cities', CityController::class);
+    
+    Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
+    Route::apiResource('/offices', OfficeSpaceController::class);
 
-Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
-Route::apiResource('/offices', OfficeSpaceController::class);
-
-Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
-Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
+    Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
+    Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
+});
